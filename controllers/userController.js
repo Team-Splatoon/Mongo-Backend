d34a7c0844584f8f7c84d3585c717333505704fe
Mackenzie Yu <e0878502@u.nus.edu>
2022-05-29 20:28:38 +0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,20 +39,20 @@ module.exports.login = async (req, res, next) => {
   try {
     const { username, email, password } = req.body
     const curruser = await User.findOne({ username })
-    const emailCheck = await User.findOne({ email })
-    const isPasswordValid = await bcrypt.compare(password, curruser.password)
 
     if (!curruser) {
       return res.json({ msg: 'Incorrect username or password.', status: false })
     }
+    const emailCheck = await User.findOne({ email })
     if (!emailCheck) {
       return res.json({ msg: 'Incorrect email.', status: false })
     }
+    const isPasswordValid = await bcrypt.compare(password, curruser.password)
     if (!isPasswordValid) {
       return res.json({ msg: 'Incorrect username or password.', status: false })
     }
-    delete user.password
-    return res.json({ status: true, user })
+    delete curruser.password
+    return res.json({ status: true, curruser })
   } catch (err) {
     next(err)
   }
