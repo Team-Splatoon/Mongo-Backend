2392dd6a937bad93dacd7623b3c3a336b51331fb
Mackenzie Yu <e0878502@u.nus.edu>
2022-07-13 21:33:46 +0800

    Update chatModel

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
-
 const {
   login,
   signup,
   getAllUsers,
   setAvatar,
-  logOut,
 } = require('../controllers/userController')
+
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/setavatar/:id', setAvatar)
